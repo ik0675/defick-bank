fa7fba6623fed9576b2f498ad7ddd60e5e415fb3
Ick <ik0675@utexas.edu>
2021-05-10 22:31:18 -0500

    minter role change function added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# coinick
+# Defick-bank
+
+Basic concept of this project is that a user gives money to a bank and a bank will give back a token which is interest about lending money.
+
+Backend
+  Blockchain
+  Smart Contracts (Openzeppelin - open source smart contract solidit library)
+    Token (Cryptocurrency) 
+    Bank
+  
+
+Frontend
+  React.js
+  Web3.js (interact with blockchain)
--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -4,18 +4,26 @@ pragma solidity >=0.6.0 <0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-  //add minter variable
+  address public minter;
 
-  //add minter changed event
+  // can be pushed notification
+  event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
-    //asign initial minter
+  constructor() public payable ERC20("Decentralized Bank Currency", "ICK") {
+    minter = msg.sender;
   }
 
-  //Add pass minter role function
+  function passMinterRole(address bank) public returns (bool) {
+    require(msg.sender == minter, 'Error, only deployer/owner can change alter minter role or transfer role to the bank');
+    minter = bank;
 
+    emit MinterChanged(msg.sender, bank);
+    return true;
+  }
+
+  // Total supply of token will increase when it is called
   function mint(address account, uint256 amount) public {
-    //check if msg.sender have minter role
+    require(msg.sender == minter, 'Error, this msg.sender is not allowed to minter');
 		_mint(account, amount);
 	}
 }
