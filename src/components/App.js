6c60590b3802a813486956baa237fd7ed17f41c4
Ick <ik0675@utexas.edu>
2021-05-19 22:26:25 -0500

    load state from Eth network before mounting

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
+import React, { Component } from 'react';
 import { Tabs, Tab } from 'react-bootstrap'
 import dBank from '../abis/dBank.json'
-import React, { Component } from 'react';
 import Token from '../abis/Token.json'
 import dbank from '../dbank.png';
 import Web3 from 'web3';
@@ -15,27 +15,47 @@ class App extends Component {
   }
 
   async loadBlockchainData(dispatch) {
-    if(typeof window.ethereum !== 'undefined') {
+    if (typeof window.ethereum !== 'undefined') {
       const web3 = new Web3(window.ethereum);
-      const net_id = await web3.eth.net.getId() || 1337;
+      const net_id = await web3.eth.net.getId();
       // key point
       await window.ethereum.enable();
-      console.log(net_id);
       const accounts = await web3.eth.getAccounts();
-      console.log(accounts);
-      // console.log(accounts[0])
+
+      if (typeof accounts[0] !== 'undefined') {
+        const balance = await web3.eth.getBalance(accounts[0]);
+        this.setState({
+          account: accounts[0],
+          balance,
+          web3
+        })
+      } else {
+        window.alert('Please login into MetaMask');
+      }
+
+      try {
+        //Token
+        const token = new web3.eth.Contract(Token.abi, Token.networks[net_id].address);
+        //Bank
+        const dbank = new web3.eth.Contract(dBank.abi, dBank.networks[net_id].address);
+        const dbank_address = dBank.networks[net_id].address;
+
+        this.setState({
+          token,
+          dbank,
+          dBankAddress: dbank_address
+        })
+        console.log(dbank_address)
+      } catch (e) {
+        console('Error', e);
+        window.alert('Contracts not deployed to the current network');
+      }
 
 
     } else {
       window.alert('Please install MetaMask to see this web')
     }
     
-
-      //assign to values to variables: web3, netId, accounts
-
-      //check if account is detected, then load balance&setStates, elsepush alert
-
-      //in try block load contracts
 
     //if MetaMask not exists push alert
   }
@@ -79,14 +99,14 @@ class App extends Component {
         </nav>
         <div className="container-fluid mt-5 text-center">
         <br></br>
-          <h1>{/*add welcome msg*/}</h1>
+          <h1> Welcome to Ick's Defi bank </h1>
           <h2>{/*add user address*/}</h2>
           <br></br>
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
               <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-                {/*add Tab deposit*/}
+                {/*add Tab deposit*/} 
                 {/*add Tab withdraw*/}
               </Tabs>
               </div>
